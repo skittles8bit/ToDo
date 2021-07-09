910e9887e9f7c4799f754b9642ab67e95ea16f4a
Alex <>
2021-07-09 04:14:32 -0700

    feat: create notes table view

---
+++ ToDo/ToDo/NoteTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  NoteTableViewCell.swift
+//  ToDo
+//
+//  Created by Alex on 7/9/21.
+//
+
+import UIKit
+
+class NoteTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    
+}
